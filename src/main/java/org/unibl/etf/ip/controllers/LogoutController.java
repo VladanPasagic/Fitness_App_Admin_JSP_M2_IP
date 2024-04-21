5e84301b6171621c200c0b708f20ee1f069d04e4
Pasha <93393767+VladanPasagic@users.noreply.github.com>
2024-04-21 18:34:04 +0200

    Added logout

---
+++ src/main/java/org/unibl/etf/ip/controllers/LogoutController.java
@@ -0,0 +1,55 @@
+package org.unibl.etf.ip.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.unibl.etf.ip.utilities.Constants.START_PAGE;
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet("/LogoutController")
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogoutController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		String address = START_PAGE + "login.jsp";
+		HttpSession session = request.getSession();
+		session.removeAttribute("user");
+		RequestDispatcher dispatcher = request.getRequestDispatcher(address);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/WEB-INF/pages/menu.jsp
+++ src/main/webapp/WEB-INF/pages/menu.jsp
@@ -2,4 +2,5 @@
 	<li><a href="CategoriesController">Categories</a></li>
 	<li><a href="UsersController">Users</a></li>
 	<li><a href="StatsController">Stats</a></li>
+	<li><a href="LogoutController">Logout</a></li>
 </menu>
